f023fb377522d989a235a3b322b4c4478cf13048
minjk-bl <mjkim-bl@gmail.com>
2021-12-04 03:08:02 +0900

    Add kernel functions

--- js/com/com_kernel.js
+++ js/com/com_kernel.js
@@ -9,8 +9,9 @@
  *    Change Date     :
  */
 define([
-    './com_Config'
-], function(com_Config) {
+    './com_Config',
+    './com_util'
+], function(com_Config, com_util) {
 
     /**
      * Kernel interface class
@@ -43,12 +44,99 @@ define([
         //====================================================================
         // Executing command api
         //====================================================================
-        execute(command, callback) {
-            
+        execute(command, isSilent = false) {
+            return new Promise(function(resolve, reject) {
+                Jupyter.notebook.kernel.execute(
+                    command,
+                    {
+                        iopub: {
+                            output: function (msg) {
+                                if (msg.content) {
+                                    if (msg.content['name'] == 'stderr') {
+                                        reject(msg);
+                                    } else {
+                                        var result = '';
+                                        var type = '';
+                                        if (msg.content['text']) {
+                                            result = String(msg.content['text']);
+                                            type = 'text';
+                                        } else if (msg.content.data) {
+                                            if (msg.content.data['text/plain']) {
+                                                result = String(msg.content.data['text/plain']);
+                                                type = 'text/plain';
+                                            } else if (msg.content.data['text/html']) {
+                                                result = String(msg.content.data['text/html']);
+                                                type = 'text/html';
+                                            }
+                                        }
+                                        resolve(result, type);
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    { silent: isSilent }
+                );
+            });
         }
 
-        getDataList(dataTypeList) {
-            return this.data;
+        getDataList(dataTypeList=[]) {
+            // use function command to get variable list of selected data types
+            var cmdSB = '_vp_print(_vp_get_variables_list(None))';
+            if (dataTypeList && dataTypeList.length > 0) {
+                cmdSB = com_util.formatString('_vp_print(_vp_get_variables_list({0}))', JSON.stringify(dataTypeList));
+            }
+            
+            var that = this;
+            return new Promise(function(resolve, reject) {
+                that.execute(cmdSB.toString()).then(function(result, type) {
+                    // resolve
+                    resolve(result, type);
+                }).catch(function(err) {
+                    // reject
+                    reject(err);
+                })
+            })
+        }
+
+        getColumnList(dataframe) {
+            var that = this;
+            return new Promise(function(resolve, reject) {
+                that.execute(com_util.formatString('_vp_print(_vp_get_columns_list({0}))', dataframe))
+                .then(function(result) {
+                    resolve(result);
+                });
+            });
+        }
+    
+        getCommonColumnList(dataframeList) {
+            var that = this;
+            return new Promise(function(resolve, reject) {
+                that.execute(com_util.formatString('_vp_print(_vp_get_multi_columns_list([{0}]))', dataframeList.join(',')))
+                .then(function(result) {
+                    resolve(result);
+                });
+            });
+        }
+    
+        getRowList(dataframe) {
+            var that = this;
+            return new Promise(function(resolve, reject) {
+                that.execute(com_util.formatString('_vp_print(_vp_get_rows_list({0}))', dataframe))
+                .then(function(result) {
+                    resolve(result);
+                });
+            });
+        }
+    
+        getProfilingList() {
+            var that = this;
+            return new Promise(function(resolve, reject) {
+                that.execute('_vp_print(_vp_get_profiling_list())')
+                .then(function(result) {
+                    resolve(result);
+                });
+            });
         }
 
         //====================================================================
