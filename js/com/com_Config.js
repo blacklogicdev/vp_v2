ff456cec049cedc6646605b99a59d684242108c9
minjk-bl <mjkim-bl@gmail.com>
2021-12-06 03:58:21 +0900

    Add get/set config data from Jupyter Config

--- js/com/com_Config.js
+++ js/com/com_Config.js
@@ -39,11 +39,57 @@ define([], function() {
             this.data = {
                 // Configuration
                 'vpcfg': {
-
+                    
                 },
                 // User defined code for Snippets
                 'vpudf': {
-
+                    'default import': [
+                        'import numpy as np',
+                        'import pandas as pd',
+                        'import matplotlib.pyplot as plt',
+                        '%matplotlib inline',
+                        'import seaborn as sns',
+                        'import plotly.express as px'
+                    ],
+                    'matplotlib customizing': [
+                        'import matplotlib.pyplot as plt',
+                        '%matplotlib inline',
+                        '',
+                        "plt.rc('figure', figsize=(12, 8))",
+                        '',
+                        'from matplotlib import rcParams',
+                        "rcParams['font.family'] = 'New Gulim'",
+                        "rcParams['font.size'] = 10",
+                        "rcParams['axes.unicode_minus'] = False"
+                    ],
+                    'as_float': [
+                        'def as_float(x):',
+                        '    """',
+                        "    usage: df['col'] = df['col'].apply(as_float)",
+                        '    """',
+                        '    if not isinstance(x, str):',
+                        '        return 0.0',
+                        '    else:',
+                        '        try:',
+                        '            result = float(x)',
+                        '            return result',
+                        '        except ValueError:',
+                        '            return 0.0'
+                    ],
+                    'as_int': [
+                        'def as_int(x):',
+                        '    """',
+                        "    usage: df['col'] = df['col'].apply(as_int)",
+                        '    """',
+                        '    if not isinstance(x, str):',
+                        '        return 0',
+                        '    else:',
+                        '        try:',
+                        '            result = int(x)',
+                        '            return result',
+                        '        except ValueError:',
+                        '            return 0.0'
+                    ]
                 }
             }
 
@@ -59,12 +105,21 @@ define([], function() {
             return Config.serverMode;
         }
 
-        getData(key) {
-            // TODO: get data using key
+        getData(dataKey, configKey='vpudf') {
+            // get data using key
+            var data = Jupyter.notebook.config.data[configKey];
+            if (Object.keys(data).length > 0) {
+                return data[dataKey];
+            }
+
+            return undefined;
         }
 
-        setData(dataObj) {
-            // TODO: set data using key
+        setData(dataObj, configKey='vpudf') {
+            // set data using key
+            Jupyter.notebook.config.loaded.then(function() {
+                Jupyter.notebook.config.update({[configKey]: dataObj});
+            });
         }
 
 
